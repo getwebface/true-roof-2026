8dfb990baea49b0071ba5ca89b12303b850c2605
Builder <deploy@example.com>
2025-12-21 22:38:36 +1100

    Add Cloudflare Pages functions bridge

---
+++ app/entry.server.tsx
@@ -0,0 +1,27 @@
+import { ReactRouterServer } from "react-router";
+import type { EntryContext } from "react-router";
+import { renderToReadableStream } from "react-dom/server";
+
+export default async function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  routerContext: EntryContext
+) {
+  const body = await renderToReadableStream(
+    <ReactRouterServer context={routerContext} url={request.url} />,
+    {
+      onError(error: unknown) {
+        console.error(error);
+        responseStatusCode = 500;
+      },
+    }
+  );
+
+  responseHeaders.set("Content-Type", "text/html");
+
+  return new Response(body, {
+    headers: responseHeaders,
+    status: responseStatusCode,
+  });
+}
